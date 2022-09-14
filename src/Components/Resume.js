cc435cbc7fc91370d9df67fb28385f0e30842790
lucafisc <lucafischer@fastmail.com>
2022-09-14 10:08:05 +0200

    Render section titles only if section is not empty

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -24,21 +24,39 @@ export default function Resume(props) {
     return <Education educationData={education} />;
   });
 
+  function checkIfEmpty(array) {
+    let isEmpty = [];
+    array.forEach((object) => {
+      const valuesArray = Object.values(object);
+      const result = valuesArray.every(
+        (value) => value === "" || value === undefined
+      );
+      isEmpty.push(result);
+    });
+
+    const areAllEmpty = isEmpty.every((value) => value === true);
+    return areAllEmpty;
+  }
+  const noWorkInfo = checkIfEmpty(workData);
+  const noEducationInfo = checkIfEmpty(educationData);
+  const noContactData = checkIfEmpty([contactData]);
   return (
     <div className="resume-container">
       <div style={{ backgroundColor: background }} className="resume">
         <div className="sidebar">
           {aboutData.length > 0 && <About aboutData={aboutData} />}
-          <SidebarSection title="contact" fields={contactData} />
+          {!noContactData && (
+            <SidebarSection title="contact" fields={contactData} />
+          )}
           {Object.keys(skillsData).length > 0 && (
             <SidebarSection title="skills" fields={skillsData} />
           )}
         </div>
         <div className="main">
           <Cover formData={formData} />
-          {workData.length > 0 && <SectionTitle title={"work"} />}
+          {!noWorkInfo && <SectionTitle title={"work"} />}
           {workDivs}
-          {educationData.length > 0 && <SectionTitle title={"education"} />}
+          {!noEducationInfo && <SectionTitle title={"education"} />}
           {educationDivs}
         </div>
       </div>
