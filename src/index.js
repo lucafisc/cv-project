059b2eec756911342896cc376d200957ac585f70
lucafisc <lucafischer@fastmail.com>
2022-09-30 14:18:47 +0200

    Add context provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { createContext } from "react";
+import { useContext } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 document.title = "CV Generator";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const ThemeContext = createContext();
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext.Provider value={"light"}>
+      <App />
+    </ThemeContext.Provider>
   </React.StrictMode>
 );
